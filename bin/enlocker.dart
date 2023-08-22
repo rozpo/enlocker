b7d461ccad99e4997ad822cbaf2d8f998ebe0ce3
Piotr Rozpończyk <rozpo.dev@gmail.com>
2023-08-22 11:33:24 +0100

    Create base class for runner

--- bin/enlocker.dart
+++ bin/enlocker.dart
@@ -1,1 +1,13 @@
-void main(List<String> arguments) {}
+import 'package:cli_completion/cli_completion.dart';
+
+void main(List<String> args) {
+  EnlockerCli().run(args);
+}
+
+class EnlockerCli extends CompletionCommandRunner {
+  EnlockerCli()
+      : super(
+          'enlocker',
+          'Encryption capabilities for your sensitive data.',
+        );
+}
