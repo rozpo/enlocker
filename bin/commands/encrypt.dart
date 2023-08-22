3034b3b5d45f7f4e8c95d64c2ac3b8c1a66a0d99
Piotr Rozpończyk <rozpo.dev@gmail.com>
2023-08-22 15:06:25 +0100

    Prompt user for key to be used in encrypt command

--- bin/commands/encrypt.dart
+++ bin/commands/encrypt.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:args/command_runner.dart';
+import 'package:dcli/dcli.dart';
 import 'package:encrypt/encrypt.dart';
 
 class EncryptCommand extends Command {
@@ -21,11 +22,16 @@ class EncryptCommand extends Command {
       throw usageException('Too many arguments for encrypt.');
     }
 
+    final key = ask(
+      'Key:',
+      hidden: true,
+      required: true,
+    );
+
     final text = argResults!.rest.first;
-    final key = Key.fromUtf8('my 32 length key................');
     final iv = IV.fromLength(16);
 
-    final encrypter = Encrypter(AES(key));
+    final encrypter = Encrypter(AES(Key.fromUtf8(key)));
 
     final encrypted = encrypter.encrypt(text, iv: iv);
     final decrypted = encrypter.decrypt(encrypted, iv: iv);
